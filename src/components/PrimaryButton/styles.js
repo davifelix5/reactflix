8464060a8a7eaa282028eded448c96460a6a3b99
Davi Félix <davi.felix.lima@gmail.com>
2020-07-31 13:54:43 -0300

    Created component for primary button

---
+++ src/components/PrimaryButton/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PrimaryBtn = styled.button`
+  cursor: pointer;
+  margin-bottom: 40px;
+  color: var(--white);
+  background-color: var(--primary);
+  border: none;
+  outline: none;
+  padding: 10px 30px;
+  border-radius: 4px;
+  font-size: 21px;
+  transition: all 0.3s;
+  &:hover {
+      opacity: 0.8;
+  }
+`;
