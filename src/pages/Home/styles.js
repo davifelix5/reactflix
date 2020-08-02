2b774761cbacd487f54096418ff95a9de660df56
Davi Félix <davi.felix.lima@gmail.com>
2020-08-02 18:14:56 -0300

    Adding not found message to home page

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const NotFoundMessage = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+NotFoundMessage.Content = styled.p`
+    font-size: 30px
+
+`;
